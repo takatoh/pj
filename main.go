c53aba133770db1bd299357f68e25b873271e737
takatoh <takatoh.m@gmail.com>
2017-11-28 11:27:26 +0900

    main.go: 'prettyJson' function is new.

--- main.go
+++ main.go
@@ -8,6 +8,17 @@ import (
 	"io/ioutil"
 )
 
+func prettyJson(src []byte) string {
+	buf := make([]byte, 0)
+	dst := bytes.NewBuffer(buf)
+	err := json.Indent(dst, src, "", "  ")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	return dst.String()	
+}
+
 func main() {
 	infile := os.Args[1]
 	src, err := ioutil.ReadFile(infile)
@@ -15,12 +26,6 @@ func main() {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-	buf := make([]byte, 0)
-	dst := bytes.NewBuffer(buf)
-	err = json.Indent(dst, src, "", "  ")
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
-	fmt.Println(dst.String())
+	pretty := prettyJson(src)
+	fmt.Println(pretty)
 }
